d73947ac0a142a7bd4e76d96c7eb10faeeb30b2c
jcbib <jonbibat@gmail.com>
2020-06-18 13:08:29 -0700

    Completed tutorial for lesson 16

--- my-app/src/Dummy.js
+++ my-app/src/Dummy.js
@@ -1,19 +1,52 @@
 import React from 'react';
 
-function Dummies(props) {
-    const{ dummies } = props;
-    const dummyList = dummies.map(dummy =>{
-        return ( // Needs unique key for each item in list (id)
-            <div className="dummy" key={dummy.id}>
-                <div>Name: {dummy.name}</div>
-                <div>Age: {dummy.age}</div>
-                <div>Major: {dummy.major}</div>
-            </div>
-        )
-    })
+const Dummies = ({dummies}) => {
+    // const dummyList = dummies.map(dummy =>{
+    //     if (dummy.age > 20) {
+    //         return ( // Needs unique key for each item in list (id)
+    //             <div className="dummy" key={dummy.id}>
+    //                 <div>Name: {dummy.name}</div>
+    //                 <div>Age: {dummy.age}</div>
+    //                 <div>Major: {dummy.major}</div>
+    //             </div>
+    //         )
+    //     } else {
+    //         return null;
+    //     }
+    // })
+
+    // const dummiesList = dummies.map(dummy => {
+    //     return dummy.age > 20 ? (
+    //         <div className="dummy" key={dummy.id}>
+    //             <div>Name: {dummy.name}</div>
+    //             <div>Age: {dummy.age}</div>
+    //             <div>Major: {dummy.major}</div>
+    //         </div>
+    //     ) : null;
+    // })
+    
+    // return ( 
+    //     <div className="dummy-list">
+    //         {dummiesList}
+    //     </div>
+    // )
+
+
     return (
         <div className="dummy-list">
-            {dummyList}
+            {
+                // Replaced {dummiesList} with the code assigned to dummiesList
+                // Can use ternary operator for cleaner code
+                dummies.map(dummy => {
+                    return dummy.age > 20 ? (
+                        <div className="dummy" key={dummy.id}>
+                            <div>Name: {dummy.name}</div>
+                            <div>Age: {dummy.age}</div>
+                            <div>Major: {dummy.major}</div>
+                        </div>
+                    ) : null;
+                })
+            }
         </div>
     )
 }
